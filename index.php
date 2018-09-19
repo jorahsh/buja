f2a97bd2ea680a60b38f902b3736169624079123
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-18 22:32:00 -0600

    php test3

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 	require('vendor/autoload.php');
-	require('head.php');
-	require('nav.php');
-	require('body.php');
-	require('foot.php');
-<html>
+	include('head.php');
+	include('nav.php');
+	include('body.php');
+	include('foot.php');
+#<html>
 	
-	<link rel="stylesheet" type="text/css" href="buja.css"/>
-	<head>
-		<title>Buja.com</title>
-	</head>
-	<div class="nav-bar">
-		<div class="vertical-center">
-			<img class="nav-logo" src="./img/logo-small.png">
-			<img class="menu-icon" src="./img/menu-icon1.png">
-			<img class="user-icon" src="./img/user-icon1.png">
-		</div>
-	</div>
-	<body>
-		<p>Hello world!</p>
-	</body
-<?php
-	echo '<p>this is php</p>'
-?>
-</html>
+#	<link rel="stylesheet" type="text/css" href="buja.css"/>
+#	<head>
+#		<title>Buja.com</title>
+#	</head>
+#	<div class="nav-bar">
+#		<div class="vertical-center">
+#			<img class="nav-logo" src="./img/logo-small.png">
+#			<img class="menu-icon" src="./img/menu-icon1.png">
+#			<img class="user-icon" src="./img/user-icon1.png">
+#		</div>
+#	</div>
+#	<body>
+#		<p>Hello world!</p>
+#	</body
+#<?php
+#	echo '<p>this is php</p>'
+	#?>
+#</html>
