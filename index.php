7d5a469b531c6c9ad2de2ec51ee6893424420737
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-18 22:10:40 -0600

    updated composer

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	require('vendor/autoload.php');
+?>
+<html>
+	
+	<link rel="stylesheet" type="text/css" href="buja.css"/>
+	<head>
+		<title>Buja.com</title>
+	</head>
+	<div class="nav-bar">
+		<div class="vertical-center">
+			<img class="nav-logo" src="./img/logo-small.png">
+			<img class="menu-icon" src="./img/menu-icon1.png">
+			<img class="user-icon" src="./img/user-icon1.png">
+		</div>
+	</div>
+	<body>
+		<p>Hello world!</p>
+	</body
+<?php
+	echo "this is php"
+?>
+</html>
