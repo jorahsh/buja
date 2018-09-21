f040876e40d26bd6aa72f738ff76543b3d63ab10
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-20 21:23:32 -0600

    test linking pages5

--- login_handler.php
+++ login_handler.php
@@ -1,5 +1,4 @@
 <?php
 $_POST
-	echo "<p>http://$_SERVER['HTTP_HOST']$_SERVER['REQUEST_URI']</p>";
-	header('Location: main.php');
+	header("Location: http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}/main.php");
 exit;
