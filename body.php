7b120b9a4c5cf9e8fd16bb05fce4748c20aadcc1
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-20 20:58:55 -0600

    added forms for login

--- body.php
+++ body.php
@@ -7,6 +7,14 @@ echo '<body>
 		<h3>Don\'t know what to watch?</h3>
 		<p>Buja is the world\'s leading provider in Netflix
 		recommendations, and we want to tell you what to watch!</p>
+		<form method="post" action="login_handler.php">
+			username: <input type="text" name="uname"><br>
+			passowrd: <input type="text" name="password"><br>
+			<input type="submit" value="submit">
+		</form>
+		<form method="post" action="create_account_handler.php">
+			<input type="submit" value="create account">
+		</form>
 	<div>
 	</body>';
 
