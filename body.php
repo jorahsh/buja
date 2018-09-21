61a878c4b7a5548b40f7f711cc31309d48887f21
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-20 20:52:45 -0600

    updated body of landing page

--- body.php
+++ body.php
@@ -4,6 +4,9 @@ echo '<body>
 		<div class="welcome-box-header">
 			<span>Welcome to Buja friend!</span>
 		<div>
+		<h3>Don\'t know what to watch?</h3>
+		<p>Buja is the world\'s leading provider in Netflix
+		recommendations, and we want to tell you what to watch!</p>
 	<div>
 	</body>';
 
