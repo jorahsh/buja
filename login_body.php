db45b0f9585e802598cf3d35b35456aabfc6ffb2
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-23 19:42:41 -0600

    changing background color and text size

--- login_body.php
+++ login_body.php
@@ -4,7 +4,7 @@ echo '<body>
 		<div class="welcome-box-header">
 			<span>Welcome to Buja friend!</span>
 		<div>
-		<h3>Don\'t know what to watch?</h3>
+		<h3 class="welcom-box-title">Don\'t know what to watch?</h3>
 		<p>Buja is the world\'s leading provider in Netflix
 		recommendations, and we want to tell you what to watch!</p>
 		<form method="post" action="login_handler.php">
