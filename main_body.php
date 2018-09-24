5e8a0e9683d704da666cf3f839ab0c0c5fdf206b
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-23 21:01:20 -0600

    added movie poster

--- main_body.php
+++ main_body.php
@@ -2,6 +2,7 @@
 	echo '<div class="main-container">
 			<img src="./img/left-arrow-circle.png">
 			<div class="movie-poster-box">
+				<img src="./img/star-wars-poster.jpg"
 			</div>
 			<img src="./img/right-arrow-circle.png">
 		</div>
