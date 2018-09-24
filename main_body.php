a8efcda1bbdc8f540733a3ca9ddd13a3b291ecee
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-23 20:23:08 -0600

    updating main page

---
+++ main_body.php
@@ -0,0 +1,9 @@
+<?php
+	echo '<div class="main-body">
+			<img src="./img/left-arrow-circle.png">
+			<div class="movie-poster-box">
+			</div>
+			<img src="./img/right-arrow-circle.png">
+		</div>
+		<div class="genre-bar">
+		</div>
