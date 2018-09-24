135fab72679758c353cdf2dca5f888a63a9b1834
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-23 21:25:48 -0600

    styling main page

--- main_body.php
+++ main_body.php
@@ -1,7 +1,7 @@
 <?php
 	echo '<div class="main-container">
 			<img class="left-arrow" src="./img/left-arrow-circle.png">
-			<img src="./img/star-wars-poster.jpg">
+			<img class="movie-poster" src="./img/star-wars-poster.jpg">
 			<img class="right-arrow" src="./img/right-arrow-circle.png">
 		</div>
 		<div class="genre-bar">
