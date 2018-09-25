2964c3daaf47ff356823f4e4069be65c54cce409
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-24 18:53:21 -0600

    experimenting with containers main.php

--- main_body.php
+++ main_body.php
@@ -11,7 +11,7 @@ echo '<div class="main-container">
 	</div>
 	<div class="genre-bar">
 		<img class= "grey-arrow-right" src="./img/grey-arrow-right.png">
-		<img class= "grey-arrow-left" src="./img/grey-arrow-left.png">'
+		<img class= "grey-arrow-left" src="./img/grey-arrow-left.png">';
 
 $genres = ['comedy', 'horror', 'action', 'drama', 'animated', 'family'];
 
