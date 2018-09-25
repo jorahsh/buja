60ff93ce43dcb247436528c411e174d9118f9c8a
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-24 18:18:53 -0600

    experimenting with containers main.php

--- main_body.php
+++ main_body.php
@@ -1,13 +1,20 @@
 <?php
 echo '<div class="main-container">
-	<div class="left-arrow">
+	<div class="left-arrow-cirlce">
 		<img src="./img/left-arrow-circle.png">
 	</div>
 	<div class="movie-poster">
 		<img src="./img/star-wars-poster.jpg">
 	</div>
-	<div class="right-arrow">
+	<div class="right-arrow-circle">
 		<img src="./img/right-arrow-circle.png">
 	</div>
-	<div class="genre-bar"></div>
+	<div class="genre-bar">
+		<div class="grey-arrow-right">
+			<img src="./img/grey-arrow-right.png">
+		</div>
+		<div class="grey-arrow-left">
+			<img src="./img/grey-arrow-left.png">
+		</div>
+	</div>
 </div>';
