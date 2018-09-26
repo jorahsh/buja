02e6f2fab5a50a68a5518768aaa84640d815a66e
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-25 20:37:58 -0600

    fixing styles

--- login_body.php
+++ login_body.php
@@ -1,6 +1,6 @@
 <?php
 echo '<body>
-	<div class="main-body">
+	<div class="main-container">
 	<div class="welcome-box">
 		<div class="welcome-box-header">
 			<span class="welcome-box-header-text">Welcome to Buja friend!</span>
