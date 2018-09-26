647f1a008247ef8e2c5cdaaa329c0ea344ec14a1
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-25 22:00:21 -0600

    fixing styles

--- create_account_body.php
+++ create_account_body.php
@@ -3,15 +3,27 @@ echo '<body>
 	<div class="main-container">
 	<div class="welcome-box">
 		<div class="welcome-box-header">
-			<span>One of us...One of us...</span>
+			<span class="welcome-box-header-text>
+				One of us...One of us...
+			</span>
 		</div>
-		<h3>Signup is both easy AND painless!</h3>
-		<p>We just need a few basic facts, then you\'ll be free to use
-		buja to your heart\'s content.</p>
+		<div class="welcome-box-title">
+			<h3>Signup is both easy AND painless!</h3>
+			<p>
+			We just need a few basic facts, then you\'ll be free 
+			to use buja to your heart\'s content.
+			</p>
+		</div>
 		<form method="post" action="login_handler.php">
-			username: <input type="text" name="uname"><br>
-			passowrd: <input type="text" name="password"><br>
-			<input type="submit" value="submit">
+			<div class="username-input">
+				username: <input type="text" name="uname">
+			</div>
+			<div class="password-input">
+				passowrd: <input type="text" name="password">
+			<div>
+			<div class="submit-button">
+				<input type="submit" value="submit">
+			</div>
 		</form>
 	</div>
 	</div>
