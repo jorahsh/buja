2a3fa12b022a073c3eeb4de9aa4f627249782d1e
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-25 21:54:36 -0600

    fixing styles

--- login_body.php
+++ login_body.php
@@ -5,9 +5,13 @@ echo '<body>
 		<div class="welcome-box-header">
 			<span class="welcome-box-header-text">Welcome to Buja friend!</span>
 		</div>
-		<h3 class="welcom-box-title">Don\'t know what to watch?</h3>
-		<p>Buja is the world\'s leading provider in Netflix
-		recommendations, and we want to tell you what to watch!</p>
+		<div class="welcome-box-title">
+			<h3>Don\'t know what to watch?</h3>
+			<p>
+			Buja is the world\'s leading provider in Netflix
+			recommendations, and we want to tell you what to watch!
+			</p>
+		</div>
 		<form method="post" action="login_handler.php">
 			<div class="username-input">
 				username: <input type="text" name="uname">
