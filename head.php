bad77cd7ed3189cd7f85b64043fd7f45f9987a31
jorahsh <jorahhinman@u.boisestate.edu>
2018-09-25 22:33:02 -0600

    fixing styles

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <?php
 echo '<html>
 	<link rel="stylesheet" type="text/css" href="buja.css"/>
-	<link rel="shortcut-icon" type="image/png" href="./img/logo-favicon.png"/>
 	<head>
 		<title>Buja.com</title>
-		</head>';
+		<link rel="shortcut-icon" type="image/png" href="./img/logo-favicon.png"/>
+	</head>';
