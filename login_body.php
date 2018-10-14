6a58db057ee00dab313db35f5b77618d20c4e70d
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-13 21:30:58 -0600

    first steps toward varification

--- login_body.php
+++ login_body.php
@@ -8,8 +8,9 @@ session_start();
 	<div class="main-container">
 <?php
 
-if (!empty($_SESSION['login_error'])) {
-	foreach($_SESSION['login_error'] as $message) { ?>
+if (!empty($_SESSION['login_error'])) { ?>
+<h1>OH LAWD HE'S COMIN!</h1>
+<?php	foreach($_SESSION['login_error'] as $message) { ?>
 		<div class="login_error"><?php echo $message; ?></div>
 <?php	}
 } ?>
