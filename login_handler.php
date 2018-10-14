61be56b83d36c0a22b5491826ac3f50b07e18dbf
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-13 20:57:13 -0600

    first steps toward varification

--- login_handler.php
+++ login_handler.php
@@ -5,17 +5,20 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if (isset($username) && isset($password)) {
-	header('Location: https://stark-beyond-19703.herokuapp.com/main.php');
+if (!isset($username) {
+	$_SESSION['login_error'][] = 'please enter a username';
+}
+
+if (!isset($password) {
+	$_SESSION['login_error'][] = 'please enter a password';
+}
+
+if (isset($_SESSION['login_error'])) {
+	header('Location: https://stark-beyond-19703.herokuapp.com');
 }
 else {
-	if (!isset($username) {
-		$_SESSION['login_error'][] = 'please enter a username';
-	}
-	if (!isset($password) {
-		$_SESSION['login_error'][] = 'please enter a password';
-	}
-	header('Location: https://stark-beyond-19703.herokuapp.com');
+	header('Location: https://stark-beyond-19703.herokuapp.com/main.php');
 }
 
 exit;
+?>
