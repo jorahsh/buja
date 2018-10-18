4ecb89949fbc37500cb8ce0333cb31cf8ceb1cf0
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-17 20:51:43 -0600

    more database work

--- Buja_Dao.php
+++ Buja_Dao.php
@@ -18,7 +18,7 @@ class Buja_Dao {
 
 	public function getConnection() {
 		try {
-			 echo "mysql: host={$this->hostname};dbname={$this->database}" ",{$this->username}, {$this->password}";
+			 echo "mysql: host={$this->hostname};dbname={$this->database}, {$this->username}, {$this->password}";
 			$conn = new PDO("mysql: host={$this->hostname};dbname={$this->database}", $this->username, $this->password);
 		}
 		catch (Exception $e) {
