05ea26ad58553958af4350ce414578d0fae945d4
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-20 16:35:51 -0600

    pdo finally works!

--- main_body.php
+++ main_body.php
@@ -9,17 +9,17 @@ if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 } ?>
 <div class="main-container">
 	<div class="left-arrow-circle">
-		<img src="./img/left-arrow-circle.png">
+		<input type="image" src="./img/left-arrow-circle.png">
 	</div>
 	<div class="movie-poster">
-		<img src="./img/star-wars-poster.jpg">
+		<input type="image" src="./img/star-wars-poster.jpg">
 	</div>
 	<div class="right-arrow-circle">
-		<img src="./img/right-arrow-circle.png">
+		<input type="image" src="./img/right-arrow-circle.png">
 	</div>
 	<div class="genre-bar">
-		<img class= "grey-arrow-right" src="./img/grey-arrow-right.png">
-		<img class= "grey-arrow-left" src="./img/grey-arrow-left.png">
+		<input type="image" class= "grey-arrow-right" src="./img/grey-arrow-right.png">
+		<input type="image" class= "grey-arrow-left" src="./img/grey-arrow-left.png">
 		<ul class="genre-list">';
 <?php
 foreach($genres as $genre){
