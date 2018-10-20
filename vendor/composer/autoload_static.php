9647031f50850ef1623ce1b80209769b767e0f34
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-20 15:00:03 -0600

    getting sick of mysql

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,38 +6,9 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit88dcb76519a6295a20da1c0130f0c85f
 {
-    public static $prefixLengthsPsr4 = array (
-        'P' => 
-        array (
-            'Psr\\Log\\' => 8,
-        ),
-        'K' => 
-        array (
-            'Katzgrau\\KLogger\\' => 17,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'Psr\\Log\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
-        ),
-        'Katzgrau\\KLogger\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/katzgrau/klogger/src',
-        ),
-    );
-
-    public static $classMap = array (
-        'Katzgrau\\KLogger\\Logger' => __DIR__ . '/..' . '/katzgrau/klogger/src/Logger.php',
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit88dcb76519a6295a20da1c0130f0c85f::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit88dcb76519a6295a20da1c0130f0c85f::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit88dcb76519a6295a20da1c0130f0c85f::$classMap;
 
         }, null, ClassLoader::class);
     }
