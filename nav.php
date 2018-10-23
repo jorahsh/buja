7f6d5b4dfa5a8e6c20d3e46fca092b022f8c0444
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-22 18:22:21 -0600

    trying to make a dropdown menu to log out

--- nav.php
+++ nav.php
@@ -1,8 +1,14 @@
-<?php
-	echo '<div class="nav-bar">
-		<div class="vertical-center">
-			<img class="nav-logo" src="./img/logo-small.png">
+<div class="nav-bar">
+	<div class="vertical-center">
+		<img class="nav-logo" src="./img/logo-small.png">
+		<div class ="dropdown">
 			<img class="menu-icon" src="./img/menu-icon1.png">
-			<img class="user-icon" src="./img/user-icon1.png">
+				  <div class="dropdown-content">
+    					<a href="#">Link 1</a>
+    					<a href="#">Link 2</a>
+    					<a href="#">Link 3</a>
+				  </div>
 		</div>
-	</div>';
+		<img class="user-icon" src="./img/user-icon1.png">
+	</div>
+</div>
