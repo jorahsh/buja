33d99a55a28436b29e733bea884791aae1ff5a17
jorahsh <jorahhinman@u.boisestate.edu>
2018-10-27 15:26:38 -0600

    updated movie table

--- main_body.php
+++ main_body.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 		<input type="image" src="./img/left-arrow-circle.png">
 	</div>
 	<div class="movie-poster">
-		<p><?php echo '"' . $htmlentities($movies[$rand]['title']) . '"'; ?></p>
+		<p><?php echo '"' . htmlentities($movies[$rand]['title']) . '"'; ?></p>
 		<input type="image" src=<?php echo '"'. $movies[$rand]['poster'] . '"';?>>
 	</div>
 	<div class="right-arrow-circle">
