6d257ceb17d44b7de2490a865f5d35454c57678d
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-02 22:56:25 -0600

    oh lawd he's comin!

--- create_account_handler.php
+++ create_account_handler.php
@@ -11,13 +11,22 @@ $password = $_POST['create_password'];
 if (empty($username)) {
 	$_SESSION['create_error'][] = 'please enter a username';
 }
+elseif (strlen($username) < 3) {
+	$_SESSSION['create_error'][] = 'username must be at least 3 characters';
+}
 
 if (empty($email)) {
 	$_SESSION['create_error'][] = 'please enter an email';
 }
+elseif (!preg_match('^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$', $email)) {
+	$_SESSION['create_error'][] = 'please enter a valid email';
+}
 
 if (empty($password)) {
 	$_SESSION['create_error'][] = 'please enter a password';
+}
+elseif (strlen($password) < 5) {
+	$_SESSION['create_error'][] = 'passowrd must be at least 5 characters';
 }
 
 if (!isset($_SESSION['create_error'])) {
