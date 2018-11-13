b549084d4bcd8c90323f035ca255c86d65a36b9d
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-12 17:59:47 -0700

    jquery?

--- head.php
+++ head.php
@@ -4,5 +4,7 @@ echo '<html>
 	<head>
 		<title>Buja.com</title>
 		<link rel="shortcut icon" type="image/png" href="favicon.png"/>
-		<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
+		<script src="buja_jquery.js"></script> 
 	</head>';
