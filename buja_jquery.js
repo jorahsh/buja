e3c2edd115c7d755ebea30c1744a1efd279d84b5
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-12 18:44:20 -0700

    jquery?

--- buja_jquery.js
+++ buja_jquery.js
@@ -1,27 +1,7 @@
 $(document).ready(function(){
-$('#poster_carousel').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
+	$('#poster_carousel').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3
+	});
 });
