48b9b854e3e265ebcf78625c51550502f3a2ff59
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-14 18:33:51 -0700

    carousel ride!

--- head.php
+++ head.php
@@ -5,5 +5,6 @@ echo '<html>
 		<title>Buja.com</title>
 		<link rel="shortcut icon" type="image/png" href="favicon.png"/>
 		<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+		<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css"/>
 	</head>';
--- main_body.php
+++ main_body.php
@@ -134,6 +134,6 @@ foreach($genres as $genre){
 		</ul>
 	</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
 <script src="buja_jquery.js"></script> 
 </div>
