884071cf255683c307e0cfe262c200f1b5aef8fd
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-17 23:01:12 -0700

    maybe jquery one day

--- main_body.php
+++ main_body.php
@@ -84,7 +84,7 @@ $comments = $c_dao->getMovieComments($movies[$pos]['id']);
 <?php
 	for($i = 0; $i < 8; $i++) { ?>
 			<li>
-				<img src=<?php echo '"'.$movies[$i]['poster'].'"';?> alt="" />
+				<img src="sad_jack.jpg" alt="" />
 			</li>
 <?php } ?>
 			<img src="./img/left-arrow-circle.png" class="left">
