8a4c46cba8b416ff2426d2104b395dc9d9531b81
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-17 19:07:09 -0700

    maybe jquery one day

--- head.php
+++ head.php
@@ -5,6 +5,7 @@
 		<link rel="shortcut icon" type="image/png" href="favicon.png"/>
 		<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="carousel-js/js/Carousel.js"></script>
 	<!--	<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
 		<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css"/>
 	-->
