da6102158cca36968d8798692f140a2e1523e31f
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-17 19:40:02 -0700

    maybe jquery one day

--- head.php
+++ head.php
@@ -4,6 +4,7 @@
 		<title>Buja.com</title>
 		<link rel="shortcut icon" type="image/png" href="favicon.png"/>
 		<link href="https://fonts.googleapis.com/css?family=Bevan" rel="stylesheet">
+<?php include "jquery.php"; ?>
 	<!--	<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick.css"/>
 		<link rel="stylesheet" type="text/css" href="slick-1.8.1/slick/slick-theme.css"/>
 	-->
