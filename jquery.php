cc63da419236c80e44fe6ef23f11fd5cf70deb06
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-17 19:24:08 -0700

    maybe jquery one day

--- jquery.php
+++ jquery.php
@@ -1,2 +1,3 @@
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="carousel-js/js/Carousel.js"></script>
+<script src="buja_jquery.js"></script>
