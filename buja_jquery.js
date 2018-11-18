075e72ab14538a5e9225fa7e2546c0594c173857
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-17 21:21:01 -0700

    maybe jquery one day

--- buja_jquery.js
+++ buja_jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
         $('.container').carousel({
             num: 5,
             maxWidth: 450,
-            maxHeight: 300,
+            maxHeight: 350,
             distance: 50,
             scale: 0.6,
             animationTime: 1000,
