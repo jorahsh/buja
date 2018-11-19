25a45e2caa9e7bbed37e1f747ff19cb4654b940c
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-18 17:48:25 -0700

    dang nabbit

--- jquery.php
+++ jquery.php
@@ -1,5 +1,5 @@
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script type="text/javascript" src="slick/slick.min.js"></script>
+<script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
 <script src="js/Carousel.js"></script>
 <script src="buja_jquery.js"></script>
