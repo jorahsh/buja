cf697798a60740560a9a3f66f9a451cb130c3cd7
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-24 22:39:44 -0700

    almost there

--- buja_jquery.js
+++ buja_jquery.js
@@ -74,7 +74,8 @@ $(document).ready(function(){
 	});
 
 	$('.select-genre').click(function() {
-		$.post('/main_handler.php', $('genre_tab').data('genre'), function(status) {
-			alert(status)});
+		$.post('/main_handler.php', $('genre_tab').data('genre'), function(data, status) {
+			alert(status);
+		});
 	});
 });
