40c51b24d8556f0daaba65b2df0d6ed5839ec5b9
jorahsh <jorahhinman@u.boisestate.edu>
2018-11-26 19:00:07 -0700

    I'm working on my website in class

--- jquery.php
+++ jquery.php
@@ -1,6 +1,7 @@
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
 <script type="text/javascript" src="slick-1.8.1/slick/slick.min.js"></script>
 <script src="js/Carousel.js"></script>
-<script src="buja_jquery.js"></script>
 <script type="text/javascript" src="tooltipster/dist/js/tooltipster.bundle.min.js"></script>
+<script src="buja_jquery.js"></script>
